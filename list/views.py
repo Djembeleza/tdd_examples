ca3472e4975eeca75929dbf9f62eec3936f95285
kkaola023 <kkaola023@student.wethinkcode.co.za>
2023-11-15 13:27:01 +0200

    first commit

---
+++ list/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+
+from .models import (Item)
+
+def home_page(request):
+    if request.method == "POST":
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect("/")
+    
+    return render(request, "home.html", {'items': Item.objects.all()})
